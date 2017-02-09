7e58906edca9454a368ae727c2f0c77c2a6a7130
francoiskha <francois.kha@gmail.com>
2017-02-09 23:30:39 +0100

    je fixe la version de truffle à 2

--- tooling/provision.sh
+++ tooling/provision.sh
@@ -5,10 +5,10 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
 sudo apt-get update
 # Git, node et le code	
 sudo apt-get -y install git nodejs node-gyp
-sudo npm i -g truffle
-sudo npm i -g ethereumjs-testrpc
-sudo npm i -g gulp-cli
-sudo npm i -g gulp 
+sudo npm i -g truffle@2.1.1
+sudo npm i -g ethereumjs-testrpc@3.0.3
+sudo npm i -g gulp-cli@3.9.1
+sudo npm i -g gulp@3.9.1 
 git clone https://github.com/benjaminfontaine/codelab-ethereum.git
 cd codelab-ethereum/horse-bet 
 npm i -D
