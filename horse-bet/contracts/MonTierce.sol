3c396ebff08fe9f7edbce20a0e4bb682e79271e8
francoiskha <francois.kha@gmail.com>
2017-05-09 13:47:45 +0200

    récup de TU plus aboutis du master

--- horse-bet/contracts/MonTierce.sol
+++ horse-bet/contracts/MonTierce.sol
@@ -5,44 +5,66 @@ import "mortal.sol";
 
 contract MonTierce is mortal{
 
-  //INFO : il manque le mot clé pour définir une structure de donnée
-  struct Course {
-    uint idCourse;
-    bool terminee;
-    //les chevaux sont représentés par leur id
-    uint32[] chevauxEnCourse;
-  }
-
-  uint public courseIDGenerator = 0;
-  //INFO définir une structure de données type Map qui va associer un id à une course.
-  // http://solidity.readthedocs.io/en/develop/types.html
-  // INDICE : MAP...G
-  mapping (uint => Course) courses;
-
-  //event qui va permettre de debugger le contrat au cours de test et au cours de la vie du contrat.
-  event InitialisationCourse(uint32[] chevauxAuDepart, uint idCourse, address owner);
-
-  //INFO : on souhaite restreindre l'accès à cette fonction au propriètaire du contrat
-  // on peut le faire via le modifier défini dans le fichier owned.sol
-  // https://github.com/ethereum/wiki/wiki/Solidity-Features#user-content-function-modifiers
-  function initialiserCourse(uint32[] chevauxParticipants) onlyowner returns(uint) {
-
-    //les struct Course du mapping courses sont déjà initialisés, il suffit juste de leur positionner des attributs
-    //L'initialisation suivante ne fonctionne pas
-    //Course course = Course({idCourse:courseIDGenerator, montantTotalMises:0,  terminee:false, chevauxEnCourse:chevauxParticipants });
-    //car solidity ne gère pas l'initialisation de tableaux vides
-    courses[courseIDGenerator].idCourse= courseIDGenerator;
-
-    //INFO : on doit copier les chevauxParticipants dans le storage du contrat
-    // => courses[courseIDGenerator].chevauxEnCourse
-    // INDICE : on ne peut pas copier le tableau chevauxParticipants
-    //directement de la callstack dans l'espace storage, le code est en spoiler dans le README.md
-    for(uint x= 0; x< chevauxParticipants.length; x++ ){
-      courses[courseIDGenerator].chevauxEnCourse.push(chevauxParticipants[x]);
+    //les différents état que peut prendre un pari
+    enum EtatPari { NonDetermine, Perdant, GagnantTierce, GagnantDuo, GagnantUno }
+  
+    //représente un pari
+    struct Pari {
+      address adresseParieur;
+      uint mise;
+      uint32[3] chevauxTierce;
+      EtatPari etat;
     }
-    InitialisationCourse(chevauxParticipants, courses[courseIDGenerator].idCourse, owner);
+  
+  
+    struct Course {
+      uint idCourse;
+      uint montantTotalMises;
+      bool terminee;
+      bool parisBloques;
+      //les chevaux sont représentés par leur ids
+      uint32[] chevauxEnCourse;
+      //on ne peut pas itérer sur le mapping paris sans ça
+      address[] parisKeySet;
+      mapping (address => Pari) paris;
+      mapping (uint8 => uint8) coefficientsPrime;
+      uint sommeCoefficientsParMises;
+      uint misesDesPerdants;
+      bool existeGagnantTierce;
+      bool existeGagnantDuo;
+      bool existeGagnantUno;
+    }
+  
+    uint public courseIDGenerator = 1;
+    //structure de données qui référence les courses
+    mapping (uint => Course) courses;
+  
+    //event qui va permettre de debugger le contrat au cours de test et au cours de la vie du contrat.
+    event InitialisationCourse(uint32[] chevauxAuDepart, uint idCourse, address owner);
+  
+    function initialiserCourse(uint32[] chevauxParticipants) onlyowner returns(uint) {
+  
+      //les struct Course du mapping courses sont déjà initialisés, il suffit juste de leur positionner des attributs
+      //L'initialisation suivante ne fonctionne pas
+      //Course course = Course({idCourse:courseIDGenerator, montantTotalMises:0,  terminee:false, chevauxEnCourse:chevauxParticipants });
+      //car solidity ne gère pas l'initialisation partielle d'une struct
+      courses[courseIDGenerator].idCourse= courseIDGenerator;
+      courses[courseIDGenerator].montantTotalMises=0;
+      courses[courseIDGenerator].terminee=false;
+      courses[courseIDGenerator].parisBloques=false;
+      courses[courseIDGenerator].misesDesPerdants=0;
+      for(uint x= 0; x< chevauxParticipants.length; x++ ){
+        courses[courseIDGenerator].chevauxEnCourse.push(chevauxParticipants[x]);
+      }
+      InitialisationCourse(chevauxParticipants, courses[courseIDGenerator].idCourse, owner);
+  
+      courseIDGenerator++ ;
+      return courses[courseIDGenerator].idCourse;
+    }
 
-    courseIDGenerator++ ;
-    return courses[courseIDGenerator].idCourse;
-  }
+  
+    //cette méthode, en lecture seule, permets de récupérer les informations de la course
+    function getInfosCourse(uint idCourse) public returns(uint, uint, bool, uint32[], bool){
+      // FIX_ME
+    }
 }
--- horse-bet/test/montierce.js
+++ horse-bet/test/montierce.js
@@ -1,68 +1,69 @@
-contract('MonTierce', function(accounts) {
-  
-  //account owner du contrat
-  var account_one = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1";
-  
-  //des comptes créés par testrpc, rendus fixes par l'option -d au lancement
-  var account_two = "0xffcf8fdee72ac11b5c542428b35eef5769c409f0";
-  var account_three = "0x22d491bde2303f2f43325b2108d26f1eaba1e32b";
-  var account_four = "0xe11ba2b4d45eaed5996cd0823791e0c93114882d";
-  var account_five = "0xd03ea8624c8c5987235048901fb614fdca89b117";
-  var account_six = "0x95ced938f7991cd0dfcb48f0a06a40fa1af46ebc";
-  
-  //tableau indiquant la liste des chevaux participants à la course
-  var chevauxEnCourse = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
-  it("contient une méthode initialiserCourse qui doit garder une struct Course dans le storage lorsque l'on l'appelle", function(done) {
-    
-    //INFO
-    // récupère l'interface MonTierce.sol.js
-    // à rechercher dans : http://truffle.readthedocs.io/en/latest/getting_started/contracts/#making-a-transaction
-    //syntaxe : interfaceContrat.d......d()
-    var contratTierce = MonTierce.deployed();
-    console.log(contratTierce);
-    var initOwnerOnly = false;
-    var idCourseCree = -1;
-    
-    
-    
-    //création du compte avec le bon compte (account_one est le compte par défaut)
-    // INFO : même syntaxe qu'au dessus, sans les options
-    contratTierce.initialiserCourse(chevauxEnCourse)
-        .then(function(transactionId) {
-          //une fonction générant une transaction renvoie toujours
-          // l'id de la transaction même si la fonction renvoie autre chose dans le code du contract
-          console.log("transactionId="+transactionId);
-          
-          //on veut récupérer l'id de la dernière course créée
-          //INFO : syntaxe interfaceContrat.attributPublic.call();
-          return contratTierce.courseIDGenerator.call();
-        })
-        .then(function (idCourseCompteur){
-          //idCourseCompteur est un BigInteger, on doit le convertir
-          idCourseCree = Number(idCourseCompteur - 1);
-          assert.equal(idCourseCree, 0, "L'id de la course dans le storage doit être 0");
-          //indispensable pour que le test unitaire se termine
-          done();
-        })
-        //on catche les erreurs pour effectuer le done() dans tous les cas
-        .catch(function(err){
-          console.log(err);
-          assert.fail("Une erreur inattendue s'est produite" + err.message);
-          done();
+contract('MonTierce', function (accounts) {
+
+    //account owner du contrat
+    var account_one = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1";
+
+    //des comptes créés par testrpc, rendus fixes par l'option -d au lancement
+    var account_two = "0xffcf8fdee72ac11b5c542428b35eef5769c409f0";
+    var account_three = "0x22d491bde2303f2f43325b2108d26f1eaba1e32b";
+    var account_four = "0xe11ba2b4d45eaed5996cd0823791e0c93114882d";
+    var account_five = "0xd03ea8624c8c5987235048901fb614fdca89b117";
+    var account_six = "0x95ced938f7991cd0dfcb48f0a06a40fa1af46ebc";
+
+    //tableau indiquant la liste des chevaux participants à la course
+    var chevauxEnCourse = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    it("contient une méthode initialiserCourse qui doit garder une struct Course dans le storage lorsque l'on l'appelle", function (done) {
+
+        // INFO : récupère l'interface MonTierce.sol.js
+        // http://truffle.readthedocs.io/en/latest/getting_started/contracts/#making-a-transaction
+        // vous pouvez faire un console.log dessus pour observer l'objet renvoyé
+        var contratTierce = MonTierce.deployed();
+        var initOwnerOnly = false;
+        var idCourseCree = -1;
+
+        // va nous permettre d'observer tous les événements renvoyés par la blockchain
+        var events = contratTierce.allEvents({});
+        events.watch(function (error, event) {
+            console.log(event.args);
         });
-    
-    //tente de faire une transaction de création de course sur un account autre que le owner
-    // INFO : il faut appeler la fonction d'initialisation de la course
-    // en lui passant les chevaux participants et en utilisant account_two
-    // Syntaxe de l'appel d'une fonction sur un contrat : http://truffle.readthedocs.io/en/latest/getting_started/contracts/#making-a-transaction
-    // cette appel déclenche une action d'écriture, on utilisera donc la syntaxe :
-    // interfaceContrat.nomMethode(parametres1,[ parametres2 ...],[ {options} ])
-    contratTierce.initialiserCourse(chevauxEnCourse,{'from': account_two}).catch(function(error){
-      //cette création doit échouer
-      initOwnerOnly = true;
-    }).then(function(){
-      assert.equal(initOwnerOnly, true, "La création de course doit être réservé au propriètaire du contrat.")
+
+        //création du compte avec le bon compte (account_one est le compte par défaut)
+        contratTierce.initialiserCourse(chevauxEnCourse)
+            .then(function (transactionId) {
+                //une fonction générant une transaction renvoie toujours
+                // l'id de la transaction même si la fonction renvoie autre chose dans le code du contract
+                console.log("transactionId=" + transactionId);
+
+                //on récupére l'id de la dernière course créée
+                return contratTierce.courseIDGenerator.call();
+            })
+            .then(function (idCourseCompteur) {
+                //idCourseCompteur est un BigInteger, on doit le convertir
+                idCourseCree = Number(idCourseCompteur - 1);
+                //et on recherche ensuite des informations sur cette course
+                return contratTierce.getInfosCourse.call(idCourseCree);
+            })
+            .then(function (courseDatas) {
+                assert.equal(courseDatas[0], idCourseCree, "L'id de la course dans le storage doit être 0");
+                assert.equal(courseDatas[1].valueOf(), 0, "Le montant total des paris de la course dans le storage doit être 0");
+                assert.equal(courseDatas[2], false, "La course ne doit pas être terminée");
+                //web3 renvoie des BigInteger pour les uint, il faut donc les convertir en nombre standards
+                var chevauxEnCourseRetournes = [];
+                for (var i = 0; i < courseDatas[3].length; i++) {
+                    chevauxEnCourseRetournes.push(Number(courseDatas[3][i]));
+                }
+                assert.deepEqual(chevauxEnCourseRetournes, chevauxEnCourse, "Les chevaux en course dans le storage doivent être ceux passés à l'initialisation");
+                assert.equal(courseDatas[4], false, "Les paris doivent être autorisés sur la course");
+                //indispensable pour que le test unitaire se termine
+                events.stopWatching();
+                done();
+            })
+            //on catche les erreurs pour effectuer le done() dans tous les cas
+            .catch(function (err) {
+                console.log(err);
+                assert.fail("Une erreur inattendue s'est produite" + err.message);
+                done();
+            });
     });
-  });
 });
